9c4068fe1aa3e0ecbcc29ea95ddd7fd44d522450
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-03-14 14:41:50 -0300

    Added Flashlight footer.

--- src/Resources/meme/ui/flashlight.js
+++ src/Resources/meme/ui/flashlight.js
@@ -10,14 +10,10 @@
 			height: 480,
 			width: 320
 		});
-		flashlightWindow.addEventListener('click', function(e) {
-			flashlightWindow.close(Ti.UI.createAnimation({
-				duration: 250,
-				top: 480 
-			}));
-		});
 		
 		createFlashlightWindowHeader();
+		//createFlashlightWindowResults();
+		createFlashlightWindowFooter();
 		
 		flashlightWindow.open(Ti.UI.createAnimation({
 			duration: 250,
@@ -33,6 +29,12 @@
 			top: 0,
 			left: 0,
 			visible: true
+		});
+		flashlightButton.addEventListener('click', function(e) {
+			flashlightWindow.close(Ti.UI.createAnimation({
+				duration: 250,
+				top: 480 
+			}));
 		});
 		flashlightWindow.add(flashlightButton);
 		
@@ -61,6 +63,56 @@
 			clearButtonMode: Titanium.UI.INPUT_BUTTONMODE_ONFOCUS
 		});
 		flashlightField.add(searchField);
+	};
+	
+	var createFlashlightWindowFooter = function() {
+		var tabbedBar = Ti.UI.createTabbedBar({
+			bottom: 0,
+			height: 60,
+			width: 320,
+			style: Titanium.UI.iPhone.SystemButtonStyle.BAR
+		});
+		flashlightWindow.add(tabbedBar);
+		
+		var barButtonPhoto = Titanium.UI.createButton({
+			image: 'images/flashlight_tab1.png',
+			width: 80,
+			height: 60,
+			top: 0,
+			left: 0,
+			visible: true
+		});
+		tabbedBar.add(barButtonPhoto);
+		
+		var barButtonVideo = Titanium.UI.createButton({
+			image: 'images/flashlight_tab2.png',
+			width: 80,
+			height: 60,
+			top: 0,
+			left: 80,
+			visible: true
+		});
+		tabbedBar.add(barButtonVideo);
+		
+		var barButtonText = Titanium.UI.createButton({
+			image: 'images/flashlight_tab3.png',
+			width: 80,
+			height: 60,
+			top: 0,
+			left: 160,
+			visible: true
+		});
+		tabbedBar.add(barButtonText);
+		
+		var barButtonTweet = Titanium.UI.createButton({
+			image: 'images/flashlight_tab4.png',
+			width: 80,
+			height: 60,
+			top: 0,
+			left: 240,
+			visible: true
+		});
+		tabbedBar.add(barButtonTweet);
 	};
 	
 })();
