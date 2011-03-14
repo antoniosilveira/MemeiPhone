9fb8b0d1a04e7c32d8452d13c244dc63b4d5aa2b
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-03-13 22:38:45 -0300

    Posting screen done.

--- src/Resources/meme/ui/post.js
+++ src/Resources/meme/ui/post.js
@@ -8,7 +8,46 @@
 			height: 480,
 			width: 320
 		});
-			
+		
+		var titleField = Ti.UI.createTextField({
+			hintText: 'add title',
+			textAlign: 'left',
+			verticalAlign: 'center',
+			font: { fontSize: 20, fontFamily: 'Helvetica', fontWeight: 'bold' },
+			width: 300,
+			height: 45,
+			top: 10,
+			left: 10,
+			borderStyle: Ti.UI.INPUT_BORDERSTYLE_NONE,
+			keyboardType: Ti.UI.KEYBOARD_DEFAULT,
+			clearButtonMode: Ti.UI.INPUT_BUTTONMODE_ONFOCUS
+		});
+		postWindow.add(titleField);
+		
+		var dottedLineView = Titanium.UI.createView({
+			backgroundImage: 'images/dotted_line.png',
+			top: 55,
+			left: 0,
+			width: 320,
+			height: 2
+		});
+		postWindow.add(dottedLineView);
+		
+		var textField = Ti.UI.createTextArea({
+			hintText: 'write your post here',
+			height: 378,
+			width: 320,
+			left: 0,
+			top: 57,
+			font: { fontSize: 16, fontFamily: 'Helvetica', fontWeight: 'regular' },
+			textAlign: 'left',
+			appearance: Ti.UI.KEYBOARD_APPEARANCE_ALERT,	
+			keyboardType: Ti.UI.KEYBOARD_DEFAULT,
+			clearButtonMode: Ti.UI.INPUT_BUTTONMODE_ONFOCUS,
+			//suppressReturn: false
+		});
+		postWindow.add(textField);
+		
 		var flashlightButton = Ti.UI.createButton({
 			image: 'images/post_button_flashlight.png',
 			top: 436,
