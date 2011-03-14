0da3ce47e317b683c8e203698c454be24fb0054a
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-03-14 00:26:22 -0300

    Temporarily removing secrets.js from gitignore. The secrets there are not a secret yet :)

---
+++ src/Resources/meme/config/secrets.js
@@ -0,0 +1,8 @@
+(function(){
+	
+	meme.config.secrets = {
+		oauth_consumer_key: 'dj0yJmk9MjhCb05yMTE2OVYxJmQ9WVdrOU1WUmpVMlJ4TldVbWNHbzlNVEUxTXpRek9EVTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD05Yw--',
+		oauth_consumer_secret: 'e998a65c96f468a8710e825d8b0d540339342aa0'
+	};
+	
+})();
