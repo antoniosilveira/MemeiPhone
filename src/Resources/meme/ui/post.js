81458b1e955d38afca8ba03c56ca2d7d9e36bd92
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-03-13 22:18:28 -0300

    Added images to post window.

--- src/Resources/meme/ui/post.js
+++ src/Resources/meme/ui/post.js
@@ -8,9 +8,37 @@
 			height: 480,
 			width: 320
 		});
-		postWindow.addEventListener('click', function(e) {
+			
+		var flashlightButton = Ti.UI.createButton({
+			image: 'images/post_button_flashlight.png',
+			top: 436,
+			left: 0,
+			width: 145, 
+			height: 44
+		});
+		postWindow.add(flashlightButton);
+	
+		var pictureButton = Ti.UI.createButton({
+			image: 'images/post_button_picture.png',
+			top: 436,
+			left: 145,
+			width: 69, 
+			height: 44
+		});
+		postWindow.add(pictureButton);
+		
+		var postButton = Ti.UI.createButton({
+			image: 'images/post_button_submit.png',
+			top: 436,
+			left: 214,
+			width: 106, 
+			height: 44
+		});
+		postButton.addEventListener('click', function(e) {
 			postWindow.close();	
 		});
+		postWindow.add(postButton);
+		
 		postWindow.open();
 	}
 	
