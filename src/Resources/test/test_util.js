b25a8fc8054c60f66dac73fdf8ec65e7678d8168
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-03-31 15:34:36 -0300

    Created jasmine-titanium.js plugin for Pivotal's Jasmine.

--- src/Resources/test/test_util.js
+++ src/Resources/test/test_util.js
@@ -1,33 +1,37 @@
-describe('meme.util', function() {
+(function(){
 	
-	describe('meme.util.stripHtmlEntities', function() {
-		it('should strip html entities', function() {
-			var html = '<p>this is my <span class="bold">html</span> awesome\ntext!';
-			var result = meme.util.stripHtmlEntities(html);
-			expect(result).toBe('this is my html awesome text!');
+	describe('meme.util', function() {
+
+		describe('meme.util.stripHtmlEntities', function() {
+			it('should strip html entities', function() {
+				var html = '<p>this is my <span class="bold">html</span> awesome\ntext!';
+				var result = meme.util.stripHtmlEntities(html);
+				expect(result).toBe('this is my html awesome text!');
+			});
+
+			it('should return null when strip html entities with null html', function() {
+				expect(meme.util.stripHtmlEntities(null)).toBeNull();
+			});	
 		});
 
-		it('should return null when strip html entities with null html', function() {
-			expect(meme.util.stripHtmlEntities(null)).toBeNull();
-		});	
-	});
-		
-	describe('meme.util.secondsToHms', function() {
-		it('should transform seconds to M:SS format when less than 9:59 minutes', function() {
-			expect(meme.util.secondsToHms(123)).toBe('2:03');
+		describe('meme.util.secondsToHms', function() {
+			it('should transform seconds to M:SS format when less than 9:59 minutes', function() {
+				expect(meme.util.secondsToHms(123)).toBe('2:03');
+			});
+
+			it('should transform seconds to 0:00 format when zero seconds', function() {
+				expect(meme.util.secondsToHms(0)).toBe('0:00');
+			});
+
+			it('should transform seconds to 0:00 format when null seconds', function() {
+				expect(meme.util.secondsToHms(null)).toBe('0:00');
+			});
+
+			it('should transform seconds to HH:MM:SS format when seconds represent many hours', function() {
+				expect(meme.util.secondsToHms(51072)).toBe('14:11:12');
+			});
 		});
 
-		it('should transform seconds to 0:00 format when zero seconds', function() {
-			expect(meme.util.secondsToHms(0)).toBe('0:00');
-		});
-
-		it('should transform seconds to 0:00 format when null seconds', function() {
-			expect(meme.util.secondsToHms(null)).toBe('0:00');
-		});
-
-		it('should transform seconds to HH:MM:SS format when seconds represent many hours', function() {
-			expect(meme.util.secondsToHms(51072)).toBe('14:11:12');
-		});
 	});
 	
-});
+})();
--- src/Resources/test/tests.js
+++ src/Resources/test/tests.js
@@ -1,77 +1,12 @@
 (function(){
 	if (meme.config.tests_enabled) {
-		var testWindow = Titanium.UI.createWindow({
-			title:'Application Tests',
-			backgroundColor: 'white',
-			navBarHidden: true,
-			zIndex: 999
-		});
-		testWindow.open();
-		
-		var testsWebView = Ti.UI.createWebView({
-			html: 	'',
-			top: 	5,
-		    left: 	5,
-			width: 	310,
-			height: 470
-		});
-		testWindow.add(testsWebView);
-		
-		var testResults = '';
-		var testResultsBegin = '<html><head><style type="text/css">body{font-size:10px;font-family:helvetica;}</style></head><body>';
-		var testResultsEnd = '</body></html>';
-		var updateTestResults = function(message) {
-			testResults += message;
-			testsWebView.html = testResultsBegin + testResults + testResultsEnd;
-		};
-
 		Ti.include('/test/lib/jasmine-1.0.2.js');
+		Ti.include('/test/lib/jasmine-titanium.js');
 		
 		// Include all the test files
 		Ti.include('/test/test_util.js');
 		
-	    var TitaniumReporter = function() {
-	        this.started = false;
-	        this.finished = false;
-	    };
-
-	    TitaniumReporter.prototype = {
-	        reportRunnerResults: function(runner) {
-	            this.finished = true;
-	            //this.log('<h3>Test Runner Finished.</h3>');
-	        },
-
-	        reportRunnerStarting: function(runner) {
-	            this.started = true;
-	            this.log('<h3>Test Runner Started.</h3>');
-	        },
-
-	        reportSpecResults: function(spec) {
-				var color = '#FF0000';
-	            if (spec.results().passed()) {
-	                color = '#009900';
-	            }
-
-	            //this.log('[' + spec.suite.description + '] <font color="' + color + '">' + spec.description + '</font><br>');
-				this.log('• <font color="' + color + '">' + spec.description + '</font><br>');
-	        },
-
-	        reportSpecStarting: function(spec) {
-	            //this.log('[' + spec.suite.description + '] ' + spec.description + '... ');
-	        },
-
-	        reportSuiteResults: function(suite) {
-	            var results = suite.results();
-
-	            this.log('<b>[' + suite.description + '] ' + results.passedCount + ' of ' + results.totalCount + ' passed.</b><br><br>');
-	        },
-
-	        log: function(str) {
-	            updateTestResults(str);
-	        }
-	    };
-		
-		jasmine.getEnv().addReporter(new TitaniumReporter());
+		jasmine.getEnv().addReporter(new jasmine.TitaniumReporter());
 		jasmine.getEnv().execute();
 	}
 })();
