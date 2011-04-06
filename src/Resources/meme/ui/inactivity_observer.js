d06e4ac79b3362fb621f6ad7da6f1de1236b9e07
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-04-06 17:28:42 -0300

    Added observer docs.

--- src/Resources/meme/ui/inactivity_observer.js
+++ src/Resources/meme/ui/inactivity_observer.js
@@ -5,14 +5,20 @@ The observer class allows you to observe a text or search
 field and execute actions after some period of inactivity.
 
 Usage:
-	// TODO
+	// create observer instance
+	// (you don't necessarily have to store it in a variable)
+	var observer = meme.ui.inactivityObserver({
+		fieldToObserve: searchField,
+		inactivityTimeout: 1000,
+		inactivityTimeoutAction: function() {
+			// things to do on inactivity timeout
+		}
+	});
+	
+	// if for some reason you need to pause observer
+	// (for instance, to avoid overriding user actions)
+	observer.pauseObserver();
 ***************************************************/
-
-// ##################################################
-// TODO:
-// - Encode cache keys (?)
-// - Support blob caching
-// ##################################################
 
 (function(){
 	
