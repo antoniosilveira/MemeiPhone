c9bd575b6b81fe3d6712c0fa2732c84821ec0534
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-04-06 19:27:47 -0300

    Added tests for image resizing.

--- src/Resources/test/tests/util/test_util.js
+++ src/Resources/test/tests/util/test_util.js
@@ -31,6 +31,56 @@
 				expect(meme.util.secondsToHms(51072)).toBe('14:11:12');
 			});
 		});
+		
+		describe('meme.util.getImageDownsizedSizes', function() {
+			it('should get new sizes for wide images', function() {
+				var newSizes = meme.util.getImageDownsizedSizes(500, 500, {
+					width: 1000, 
+					height: 500
+				});
+				expect(newSizes.width).toBe(500);
+				expect(newSizes.height).toBe(250);
+			});
+			
+			it('should get new sizes restricted by 4:3 format', function() {
+				var newSizes = meme.util.getImageDownsizedSizes(100, 75, {
+					width: 800, 
+					height: 2400
+				});
+				expect(newSizes.width).toBe(25);
+				expect(newSizes.height).toBe(75);
+			});
+			
+			it('should get new sizes for tall images', function() {
+				var newSizes = meme.util.getImageDownsizedSizes(500, 500, {
+					width: 200, 
+					height: 2000
+				});
+				expect(newSizes.width).toBe(50);
+				expect(newSizes.height).toBe(500);
+			});
+		});
+		
+		describe('meme.util.resizeImage', function() {
+			it('should resize images respecting max_size constraints', function() {
+				var image = {
+					width: 1000,
+					height: 500,
+					imageAsResized: null
+				};
+				
+				spyOn(image, 'imageAsResized').andReturn({
+					width: 500,
+					height: 250
+				});
+				
+				var newImage = meme.util.resizeImage(500, 500, image);
+				
+				expect(image.imageAsResized).toHaveBeenCalledWith(500, 250)
+				expect(newImage.width).toBe(500);
+				expect(newImage.height).toBe(250);
+			});
+		});
 
 	});
 	
