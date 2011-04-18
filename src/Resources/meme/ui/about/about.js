fbf34d0c66806291bf255edae35f18e12b6c5fa9
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-04-18 14:49:33 -0300

    Finished about window :)

--- src/Resources/meme/ui/about/about.js
+++ src/Resources/meme/ui/about/about.js
@@ -19,11 +19,47 @@
 			
 			aboutWindow.open({
 				transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT,
-				duration: 300
+				duration: 500
 			});
 		};
 		
 		var createHeader = function() {
+			var aboutBar = Ti.UI.createView({
+				top: 0,
+				width: 320,
+				height: 40,
+				backgroundImage: 'images/about_navbar_bg.png'
+			});
+			aboutWindow.add(aboutBar);
+			
+			var aboutLabel = Ti.UI.createLabel({
+				text: 'about',
+				color: 'white',
+				height: 22,
+				left: 60,
+				width: 200,
+				textAlign: 'center',
+				font: { fontSize: 20, fontFamily: 'Helvetica', fontWeight: 'Bold' }
+			});
+			aboutBar.add(aboutLabel);
+			
+			var doneButton = Ti.UI.createButton({
+				top: 5,
+				right: 5,
+				height: 30,
+				width: 60,
+				backgroundImage: 'images/btn_done.png',
+				title: 'done',
+				font: { fontSize: 12, fontFamily: 'Helvetica Neue', fontWeight: 'Bold' }
+			});
+			doneButton.addEventListener('click', function() {
+				aboutWindow.close({
+					transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_RIGHT,
+					duration: 500
+				});
+			});
+			aboutBar.add(doneButton);
+			
 			var logoView = Ti.UI.createView({
 				top: 40,
 				width: 320,
@@ -33,7 +69,7 @@
 			aboutWindow.add(logoView);
 			
 			var versionLabel = Ti.UI.createLabel({
-				text: 'version 1.0.0',
+				text: 'version ' + Ti.App.version,
 				color: 'white',
 				top: 59,
 				left: 100,
