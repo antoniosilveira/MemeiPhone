5c61e96d8d4d8d9cf3d85879150e157cabb55b7b
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-04-18 12:32:06 -0300

    Added about screen buttons.

--- src/Resources/meme/ui/about/about.js
+++ src/Resources/meme/ui/about/about.js
@@ -8,31 +8,70 @@
 		
 		var open = function() {
 			aboutWindow = Ti.UI.createWindow({
-				backgroundColor: 'white',
-				left: 0,
-				top: 0,
+				backgroundImage: 'images/bg.png',
 				height: 460,
 				width: 320
 			});
+			
+			createButtons();
+			
 			aboutWindow.open({
 				transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT,
 				duration: 300
 			});
-			aboutWindow.addEventListener('click', function() {
-				aboutWindow.close({
-					transition: Ti.UI.iPhone.AnimationStyle.FLIP_FROM_RIGHT,
-					duration: 300
+		};
+		
+		var createButtons = function() {
+			var buttons = {
+				tos: {
+					text: 'Yahoo! Terms of Service',
+					url: 'http://meme.yahoo.com/help/utos/?.lang=en'
+				},
+				atos: {
+					text: 'Additional Terms of Service',
+					url: 'http://meme.yahoo.com/help/atos/?.lang=en'
+				},
+				privacy: {
+					text: 'Privacy Policy',
+					url: 'http://meme.yahoo.com/help/privacy/?.lang=en'
+				},
+				guidelines: {
+					text: 'Community Guidelines',
+					url: 'http://meme.yahoo.com/help/guidelines/?.lang=en'
+				}
+			};			
+			
+			var count = 0
+			for (key in buttons) {
+				var aboutButton = Ti.UI.createButton({
+					top: 191 + (count * 62),
+					width: 320,
+					height: 62,
+					backgroundImage: 'images/about_btn.png'
 				});
-			});
+				aboutButton.addEventListener('click', function() {
+					meme.ui.openLinkOnSafari({
+						url: buttons[key].url
+					});
+				})
+				var aboutLabel = Ti.UI.createLabel({
+					text: buttons[key].text,
+					top: 20,
+					left: 26,
+					height: 20,
+					width: 260,
+					color: 'white',
+					font: { fontSize: 19, fontFamily: 'Helvetica' }
+				});
+				aboutButton.add(aboutLabel);
+				aboutWindow.add(aboutButton);
+				count++;
+			}
 		};
 		
 		return {
 			open: open
 		};
 	}();
-		
-	//http://meme.yahoo.com/help/atos/?.lang=%s
-	//http://meme.yahoo.com/help/utos/?.lang=%s
-	//http://meme.yahoo.com/help/privacy/?.lang=%s
-	//http://meme.yahoo.com/help/guidelines/?.lang=%s
+	
 })();
